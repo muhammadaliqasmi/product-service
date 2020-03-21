f8c2e872e38a30830858b4b50bc1cbeb63fa0561
Qasmi <qasmi@muhammadaliqasmi@gmail.com>
2020-03-21 16:58:17 +0500

    - Adds project's skeleton

---
+++ src/test/java/com/qasmi/market/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qasmi.market.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
